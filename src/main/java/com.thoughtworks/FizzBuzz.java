a71a2c102d0d55e823eb9c13619cd571dc447706
beichengguiyan <chihaotian@zybank.com.cn>
2020-06-15 15:31:54 +0800

    'cscs'

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -1,16 +1,16 @@
 package com.thoughtworks;
 
 public class FizzBuzz {
-    public String say(int number){
-        if(number%15==0){
+
+    public String say(int number) {
+        if (number % 15 == 0) {
             return "FizzBuzz";
-        }
-        if(number%5==0){
+        } else if (number % 5 == 0) {
             return "Buzz";
-        }
-        if(number%3==0){
+        } else if (number % 3 == 0) {
             return "Fizz";
+        } else {
+            return String.valueOf(number);
         }
-        return String.valueOf(number);
     }
 }
