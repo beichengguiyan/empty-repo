f6284ba909de732c8c9d41ac2f6b128d6a34cc89
beichengguiyan <chihaotian@zybank.com.cn>
2020-06-15 20:14:40 +0800

    little refactor

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -3,35 +3,50 @@ package com.thoughtworks;
 public class FizzBuzz {
 
     public String say(int number) {
-        String result = "";
-        if (String.valueOf(number).contains("5")) {
-            if (number % 5 == 0) {
-                result += "Buzz";
+        return contain_5(number);
+    }
+
+    public String contain_5(int number_C) {
+        String result_C = "";
+        if (String.valueOf(number_C).contains("5")) {
+            if (number_C % 5 == 0) {
+                result_C += "Buzz";
             }
-            if (number % 7 == 0) {
-                result += "Whizz";
+            if (number_C % 7 == 0) {
+                result_C += "Whizz";
             }
-            if (result.equals("")) {
-                result = String.valueOf(number);
+            if (result_C.equals("")) {
+                result_C = String.valueOf(number_C);
             }
-            return result;
-        } else {
-            if (String.valueOf(number).contains("3")) {
-                return "Fizz";
-            }
-            if (number % 3 == 0) {
-                result += "Fizz";
-            }
-            if (number % 5 == 0) {
-                result += "Buzz";
-            }
-            if (number % 7 == 0) {
-                result += "Whizz";
-            }
-            if (result.equals("")) {
-                result = String.valueOf(number);
-            }
-            return result;
+            return result_C;
         }
+        return contain_3(number_C);
+    }
+
+    public String contain_3(int number_B) {
+        if (String.valueOf(number_B).contains("3")) {
+            return "Fizz";
+        }
+        return simple_Fizz_Buzz_Whizz(number_B);
+    }
+
+    public String simple_Fizz_Buzz_Whizz(int number_A) {
+        String result_A = "";
+        if (String.valueOf(number_A).contains("3")) {
+            return "Fizz";
+        }
+        if (number_A % 3 == 0) {
+            result_A += "Fizz";
+        }
+        if (number_A % 5 == 0) {
+            result_A += "Buzz";
+        }
+        if (number_A % 7 == 0) {
+            result_A += "Whizz";
+        }
+        if (result_A.equals("")) {
+            result_A = String.valueOf(number_A);
+        }
+        return result_A;
     }
 }
--- src/test/java/com/thoughtworks/FizzBuzzTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzTest.java
@@ -88,4 +88,5 @@ public class FizzBuzzTest {
         //then
         Assert.assertThat(result,is("BuzzWhizz"));
     }
+
 }
