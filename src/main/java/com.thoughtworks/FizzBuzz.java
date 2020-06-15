304246e48262eeb94684dcc041b5e5f3cce94da6
beichengguiyan <chihaotian@zybank.com.cn>
2020-06-15 15:39:05 +0800

    'refactor'

--- src/main/java/com.thoughtworks/FizzBuzz.java
+++ src/main/java/com.thoughtworks/FizzBuzz.java
@@ -3,14 +3,17 @@ package com.thoughtworks;
 public class FizzBuzz {
 
     public String say(int number) {
-        if (number % 15 == 0) {
-            return "FizzBuzz";
-        } else if (number % 5 == 0) {
-            return "Buzz";
-        } else if (number % 3 == 0) {
-            return "Fizz";
-        } else {
-            return String.valueOf(number);
+        String result = "";
+        if (number % 3 == 0) {
+            result += "Fizz";
         }
+        if (number % 5 == 0) {
+            result += "Buzz";
+        }
+
+        if (result.equals("")) {
+            result = String.valueOf(number);
+        }
+        return result;
     }
 }
